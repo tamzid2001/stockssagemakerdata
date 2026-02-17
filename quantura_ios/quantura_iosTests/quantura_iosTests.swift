9fca50acccd0c55985f5b9c97ff8fcaedc7f7241
Tamzid Ullah <tamzid257@gmail.com>
2026-02-16 21:58:32 -0500

    Add iOS app shell and fix UIKit/bridge compile paths

---
+++ quantura_ios/quantura_iosTests/quantura_iosTests.swift
@@ -0,0 +1,16 @@
+//
+//  quantura_iosTests.swift
+//  quantura_iosTests
+//
+//  Created by Tamzid  Ullah on 2/16/26.
+//
+
+import Testing
+
+struct quantura_iosTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
